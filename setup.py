545c66b596e8100aa6d8924ca482511826a11fae
Ben Weaver <weaver@coptix.com>
2009-09-09 17:11:54 -0400

    Ignore build directories, update README with blog post, bump version number for next release.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'vecmd',
-    version = '0.1',
+    version = '0.2',
     description = 'Additional commands for virtualenv.',
     author = 'Medium',
     author_email = 'labs@thisismedium.com',
